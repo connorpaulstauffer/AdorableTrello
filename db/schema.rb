368ba23995abfca1ee223a74fcc817903e0beb21
App Academy Student <student@appacademy.io>
2015-08-10 11:19:17 -0700

    add user and session models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808010229) do
+ActiveRecord::Schema.define(version: 20150810181844) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "name",       null: false
@@ -40,5 +40,23 @@ ActiveRecord::Schema.define(version: 20150808010229) do
   end
 
   add_index "lists", ["board_id"], name: "index_lists_on_board_id"
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "user_id",       null: false
+    t.string   "session_token", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "sessions", ["session_token"], name: "index_sessions_on_session_token", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
